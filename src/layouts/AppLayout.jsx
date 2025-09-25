b5c507bfa1cdacb25f86c6dfe514c3d83e7d43ce
JoaoAlbernaz1 <joaovictoralbernaz77@gmail.com>
2025-09-25 13:37:02 -0300

    feat: add global footer component and update AppLayout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/ui/NavBar.jsx';
+import Footer from '../components/ui/Footer.jsx'; // Importe o Footer
 import '../App.css';
 
 export default function AppLayout() {
   return (
-    <div>
+    <div className="app-layout">
       <NavBar />
       <div className="app-content">
         <Outlet />
       </div>
+      {/* Footer Global - adicionado aqui */}
+      <Footer />
     </div>
   );
 }
